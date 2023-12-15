2ee0d05dd9d50169b575cc17c30f90eeab5c843e
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-12-15 09:48:16 -0600

    this is so bad

--- speedGame_Kaiser/GameViewController.swift
+++ speedGame_Kaiser/GameViewController.swift
@@ -4,12 +4,13 @@
 //
 //  Created by ANDREW KAISER on 12/7/23.
 //
-
+//NOTES- FIX IMAGEVIEWS NOT ALIGNING WITH LISTS. STORE NEW VAL"S
 import UIKit
-
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var riotLableOutlet: UILabel!
     var currentUser5: [String:String] = [:]
+    var currrent: [String:String] = [:]
     @IBOutlet weak var line1Outlet: UIImageView!
     @IBOutlet weak var line2Outlet: UIImageView!
     // USER CARD IMAGE VIEWS
@@ -23,11 +24,28 @@ class GameViewController: UIViewController {
     // pile and deck Images
     @IBOutlet weak var currentPileImageOutlet: UIImageView!
     @IBOutlet weak var deckImageOutlet: UIImageView!
-    //
+    // image view current files
+    var oneCurrentFile = String(Array(AppData.userDeck.values)[0])
+    var twoCurrentFile = String(Array(AppData.userDeck.values)[1])
+    var threeCurrentFile = String(Array(AppData.userDeck.values)[2])
+    var fourCurrentFile = String(Array(AppData.userDeck.values)[3])
+    var fiveCurrentFile = String(Array(AppData.userDeck.values)[4])
+    // notes
+    var alert: UIAlertController!
+    var okAction: UIAlertAction!
+    var riotAction: UIAlertAction!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        riotLableOutlet.isHidden = true
+        alert = UIAlertController(title: "NO!", message: "CANNOT DO THAT", preferredStyle: .alert)
+        okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        riotAction = UIAlertAction(title: "riot", style: .destructive, handler: { alert in
+            self.riotLableOutlet.text = "You are going down..."
+            self.riotLableOutlet.isHidden = false
+        })
+        alert.addAction(okAction)
+        alert.addAction(riotAction)
             line1Outlet.image = UIImage(named: "line")
             line2Outlet.image = UIImage(named: "line")
             compImageOutlet.image = UIImage(named: "evilComp")
@@ -63,48 +81,89 @@ class GameViewController: UIViewController {
     }
     
     @IBAction func cardImageOneAction(_ sender: Any) {
-        checkPlay(Array(AppData.userDeck.keys)[0], Array(AppData.userDeck.values)[0], 1)
+        checkPlay(Array(AppData.userDeck.keys)[0], Array(AppData.playingPile.keys)[0], 1)
     }
     
+    @IBAction func cardImageTwoAction(_ sender: Any) {
+        checkPlay(Array(AppData.userDeck.keys)[1], Array(AppData.playingPile.keys)[0], 2)
+    }
+    
+    @IBAction func cardImageThreeAction(_ sender: Any) {
+        checkPlay(Array(AppData.userDeck.keys)[2], Array(AppData.playingPile.keys)[0], 3)
+    }
+    
+    @IBAction func cardImageFourAction(_ sender: Any) {
+        checkPlay(Array(AppData.userDeck.keys)[3], Array(AppData.playingPile.keys)[0], 4)
+    }
+    
+    @IBAction func cardImageFiveAction(_ sender: Any) {
+        checkPlay(Array(AppData.userDeck.keys)[4], Array(AppData.playingPile.keys)[0], 5)
+    }
+    
+    
     func checkPlay(_ playingCardName: String,_ currentPlacedCard: String,_ selectedImageNum: Int) {
-        let playingCardNum = Int("\(playingCardName.first!)")!
-        let currentPlacedCardNum = Int("\(currentPlacedCard.first!)")!
-        
-        if ((playingCardNum > currentPlacedCardNum && playingCardNum < currentPlacedCardNum + 2) || (playingCardNum < currentPlacedCardNum && playingCardNum > currentPlacedCardNum - 2)) {
+        print("This is the played card:\(playingCardName) and this is the current card placed:\(currentPlacedCard)")
+        let playingCardNum = Int(getCardNums(playingCardName))!
+        let currentPlacedCardNum = Int(getCardNums(currentPlacedCard))!
+        print("This is the playingCardNum:\(playingCardNum) and this is the currentCardNum:\(currentPlacedCardNum)")
+        if ((playingCardNum > currentPlacedCardNum && playingCardNum < currentPlacedCardNum + 2) || (playingCardNum < currentPlacedCardNum && playingCardNum > currentPlacedCardNum - 2) || ((currentPlacedCardNum == 12 || currentPlacedCardNum == 13 || currentPlacedCardNum == 1) && (playingCardNum == 13 || playingCardNum == 1)) || playingCardNum == currentPlacedCardNum) {
             // play is valid
-            if var getValue = AppData.userDeck[playingCardName]{
+            print("YES!!! you can do that")
+            // add it to the playing pile
+            if let getValue = AppData.userDeck[playingCardName]{
                 print(getValue)
+                currrent[playingCardName] = "\(getValue)"
+                currentPileImageOutlet.image = UIImage(named: "\(getValue)")
             }
-           
-            if AppData.userDeck.count > 5 {
+           //Loading new card image
+           // if AppData.userDeck.count > 5 {
 //                 user had more than 5 cards, load next card
-                switch selectedImageNum {
+            AppData.userDeck[playingCardName] = nil
+            for pls in 0...5 {
+                switch pls {
                 case 1:
-                    let values = Array(AppData.userDeck.values)[5]
+                    let values = Array(AppData.userDeck.values)[0]
                     oneUserImageOutlet.image = UIImage(named: values)
                 case 2:
-                    let values = Array(AppData.userDeck.values)[5]
+                    let values = Array(AppData.userDeck.values)[1]
                     twoUserImageOutlet.image = UIImage(named: values)
                 case 3:
-                    let values = Array(AppData.userDeck.values)[5]
+                    let values = Array(AppData.userDeck.values)[2]
                     thirdUserImageOutlet.image = UIImage(named: values)
                 case 4:
-                    let values = Array(AppData.userDeck.values)[5]
+                    let values = Array(AppData.userDeck.values)[3]
                     forthUserImageOutlet.image = UIImage(named: values)
                 case 5:
-                    let values = Array(AppData.userDeck.values)[5]
+                    let values = Array(AppData.userDeck.values)[4]
                     fifthUserImageOutlet.image = UIImage(named: values)
                 default:
                     print("if i see this im scared")
                 }
-                
-            } else {
-                
             }
+                
+                print("value removed, userDeck: \(AppData.userDeck)")
+                print("currentPlaying, pile: \(AppData.playingPile)")
         } else {
             // action is invalid
-            print("cant do that")
+            present(alert, animated: true)
         }
     }
+    
+    @IBAction func noActionAction(_ sender: Any) {
+        let key = Array(AppData.leftoverDeck.keys)[0]
+        let value = Array(AppData.leftoverDeck.values)[0]
+        AppData.playingPile[key] = value
+        currentPileImageOutlet.image = UIImage(named: value)
+        AppData.leftoverDeck[key] = nil
+        print("currentPlaying, pile: \(AppData.playingPile)")
 
+    }
+    
+    func getCardNums(_ s1: String) -> String{
+      let num = s1.index(s1.startIndex, offsetBy: s1.count - 2)
+      let start = s1.index(s1.startIndex, offsetBy: 0)
+      return (String(s1[start...num]))
+    }
 }
+
+
